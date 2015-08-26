2c473cd6a87b3decfb5110ab098e984c9d871c19
Mark Vasiv <mark.vasiv@gmail.com>
2015-08-27 00:40:18 +0300

    Add today widget target

---
+++ todayWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  todayWidget
+//
+//  Created by Mark Vasiv on 27/08/15.
+//  Copyright (c) 2015 Mark Vasiv. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TodayViewController : NSViewController
+
+@end

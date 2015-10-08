9500993e3a30296f75921b568bf824fcc0f26a30
Mark Vasiv <mark.vasiv@gmail.com>
2015-10-08 23:39:17 +0300

    Update mainApp add window opacity

--- googleTranslateWidget/ViewController.m
+++ googleTranslateWidget/ViewController.m
@@ -15,17 +15,17 @@
     NSString *textForDirections = @"To use \r\r Open the notification center \r Click the Edit button at the bottom\r Find the Swipe Translate from the right menu \r Press the green + button near it \r\r Now you can translate fast";
     
     [_directionsLabel setStringValue:textForDirections];
-
+    
+    NSWindow *mainWindow=[[[NSApplication sharedApplication] windows] objectAtIndex:0];
+    [mainWindow setBackgroundColor:[NSColor clearColor]];
+    [mainWindow setOpaque:NO];
+    
     [[self view] setWantsLayer:YES];
-    [[self view].layer setBackgroundColor:[[NSColor colorWithCalibratedRed:0.79 green:0.79 blue:0.79 alpha:1] CGColor]];
     
+    [[self view].layer setBackgroundColor:[[NSColor colorWithCalibratedRed:0.79 green:0.79 blue:0.79 alpha:0.8] CGColor]];
+
     
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
-}
 
 @end

1d31764333dc6f66e4cc2a6cae978c4310f434bf
Mark Vasiv <mark.vasiv@gmail.com>
2015-10-08 17:27:03 +0300

    Update main app colour and text

--- googleTranslateWidget/ViewController.m
+++ googleTranslateWidget/ViewController.m
@@ -12,10 +12,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSString *textForDirections = [NSString new];
-    textForDirections = [NSString stringWithFormat:@"To use \r\r Open the notification center \r Click the Edit button at the bottom\r Find the tTranslator from the right menu \r Press the green + button near it \r\r Now you can translate fast"];
+    NSString *textForDirections = @"To use \r\r Open the notification center \r Click the Edit button at the bottom\r Find the Swipe Translate from the right menu \r Press the green + button near it \r\r Now you can translate fast";
     
     [_directionsLabel setStringValue:textForDirections];
+
+    [[self view] setWantsLayer:YES];
+    [[self view].layer setBackgroundColor:[[NSColor colorWithCalibratedRed:0.79 green:0.79 blue:0.79 alpha:1] CGColor]];
     
     
 }

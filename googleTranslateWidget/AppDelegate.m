3e0d27767c798771d54b3f550c731213a790603d
Mark Vasiv <mark.vasiv@gmail.com>
2015-10-08 17:30:45 +0300

    Update AppDelegate to close main App

---
+++ googleTranslateWidget/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  googleTranslateWidget
+//
+//  Created by Mark Vasiv on 27/08/15.
+//  Copyright (c) 2015 Mark Vasiv. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
+}
+
+@end

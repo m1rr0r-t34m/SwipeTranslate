52a16e95e9faea2a9a275b921d2fce6704b253b8
theFlutter <f.l.i.t.e1.9.9.6@gmail.com>
2015-10-25 00:48:05 +0300

    Labels in top of the view are changed with text fields Suggestion menu class files created

---
+++ googleTranslateWidget/LanguageSuggestionMenu.m
@@ -0,0 +1,19 @@
+//
+//  LanguageSuggestionMenu.m
+//  googleTranslateWidget
+//
+//  Created by Andrei on 25/10/15.
+//  Copyright © 2015 Mark Vasiv. All rights reserved.
+//
+
+#import "LanguageSuggestionMenu.h"
+
+@implementation LanguageSuggestionMenu
+
++(NSMenu*)createMenuWithAction:(NSString *)action andSender:(id)sender{
+    NSMenu* outputSuggestionMenu = [NSMenu new];
+    
+    return outputSuggestionMenu;
+}
+
+@end

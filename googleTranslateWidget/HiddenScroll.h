3c8d9fcf2a63691e89cf1eb0bc1944ea65e233de
Mark Vasiv <mark.vasiv@gmail.com>
2015-11-13 01:06:05 +0200

    Fix input scroll

---
+++ googleTranslateWidget/HiddenScroll.h
@@ -0,0 +1,13 @@
+//
+//  HiddenScroll.h
+//  googleTranslateWidget
+//
+//  Created by Mark Vasiv on 13/11/15.
+//  Copyright © 2015 Mark Vasiv. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HiddenScroll : NSScroller
+
+@end
